6729e6e3fa702c7d6d29a7542d476edd262703db
AndyChen <andy565866@gmail.com>
2018-05-06 17:41:27 +0800

    Movie Info prototyping.

--- Moview/routes/index.js
+++ Moview/routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/movieDetail', function(req, res, next) {
+  res.render('movieDetail');
+});
+
 module.exports = router;
