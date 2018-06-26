a23e7f2dffe83a2343a2efecb2cd44e9137001f2
AndyChen <andy565866@gmail.com>
2018-06-26 18:04:26 +0800

    Fix: localhost Url problem

--- Moview/config/auth.js
+++ Moview/config/auth.js
@@ -4,7 +4,7 @@ module.exports = {
     'facebookAuth': {
         'clientId': '1786448471377664',
         'clientSecret': '728205b805eca8b47d453d23a0aa42f0',
-        'callbackURL': 'http://localhost:3000/auth/facebook/callback',
+        'callbackURL': '/auth/facebook/callback',
         'profileFields' : ['id', 'email', 'name', 'displayName']
     }
 }
